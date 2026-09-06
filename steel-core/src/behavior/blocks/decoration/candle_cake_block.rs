4c8244c7a65f2f59d3f153a89e179b910dba2cf5
Greened <greeneddev@gmail.com>
2026-09-06 22:45:41 +0400

    chore: readd todo

--- steel-core/src/behavior/blocks/decoration/candle_cake_block.rs
+++ steel-core/src/behavior/blocks/decoration/candle_cake_block.rs
@@ -81,6 +81,7 @@ impl BlockBehavior for CandleCakeBlock {
             && state.get_value(LIT)
         {
             world.set_block(pos, state.set_value(LIT, false), UpdateFlags::UPDATE_ALL);
+            // TODO: particles!
             world.play_block_sound(
                 &sound_events::BLOCK_CANDLE_EXTINGUISH,
                 pos,
